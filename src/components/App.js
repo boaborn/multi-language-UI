d3bacf4bad6d8c72a5e6313b6b6d7736022dade8
Michael <michaelzhaojq@hotmail.com>
2020-03-08 22:51:09 +1300

    Add context consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import UserCreate from './UserCreate'
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
 
 class App extends Component {
 
@@ -18,9 +19,11 @@ class App extends Component {
           <i className="flag us" onClick={ () => this.onLanguageChange('english') }/>
           <i className="flag nl" onClick={ () => this.onLanguageChange('dutch') }/>
         </div>
-        <LanguageContext.Provider value={ this.state.language }>
-          <UserCreate/>
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={ this.state.language }>
+            <UserCreate/>
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
 
       </div>
     )
