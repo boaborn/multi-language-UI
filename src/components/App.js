c267e7a49d9777e3a60ab1507825f08b0f5b9e28
Michael <michaelzhaojq@hotmail.com>
2020-03-08 23:18:57 +1300

    Add language component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import UserCreate from './UserCreate'
 import LanguageContext from '../contexts/LanguageContext'
 import ColorContext from '../contexts/ColorContext'
+import LanguageSelector from '../components/LanguageSelector'
 
 class App extends Component {
 
@@ -14,11 +15,8 @@ class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i className="flag us" onClick={ () => this.onLanguageChange('english') }/>
-          <i className="flag nl" onClick={ () => this.onLanguageChange('dutch') }/>
-        </div>
+        <LanguageSelector onLanguageChange={ this.onLanguageChange }/>
+
         <ColorContext.Provider value="red">
           <LanguageContext.Provider value={ this.state.language }>
             <UserCreate/>
