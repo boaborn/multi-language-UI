17a0e33495a804627d1861bfcfa6e5a0c5e9a644
Michael <michaelzhaojq@hotmail.com>
2020-03-08 23:34:27 +1300

    Create context store, dont try this

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
-export default React.createContext('english')
+// Experiment
+const Context = React.createContext('english')
+
+export class LanguageStore extends React.Component {
+
+  state = { language: 'english' }
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
+  render() {
+    return (
+      <Context.Provider value={ { ...this.state, onLanguageChange: this.onLanguageChange } }>
+        { this.props.children }
+      </Context.Provider>
+    )
+  }
+}
+export default Context
+// Standard
+// export default React.createContext('english')
 
 // Debug code
 // const context = React.createContext('english')
